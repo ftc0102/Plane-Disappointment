7e1f8f58a4dd7cf243e25ace59efafb8f0173b45
Franny-Dancis <42708672+Franny-Dancis@users.noreply.github.com>
2021-05-03 11:38:12 -0700

    Added  keyboard sound to backspace key

--- src/scenes/getName.js
+++ src/scenes/getName.js
@@ -26,6 +26,7 @@ class getName extends Phaser.Scene {
             //A document that explains each unique keycode: https://github.com/photonstorm/phaser/blob/v3.51.0/src/input/keyboard/keys/KeyCodes.js 
             
             if (event.keyCode === 8 && playerInput.text.length > 0) { //this is the backspace key; to delete the typed text from playerInput.text string
+                typingSound.play();
                 playerInput.text = playerInput.text.substr(0, playerInput.text.length - 1);
             }
             else if (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode <= 90)) { // this is to add space and all upper and lower captial letters to the playerInput.text string
