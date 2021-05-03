d42834220113ea13748dd4b39fcbd90f314fee62
Fion Kwok <fikwok@ucsc.edu>
2021-05-02 21:25:56 -0700

    Working on how to grab data from playerInput.text

--- src/scenes/getPlayerInfo.js
+++ src/scenes/getPlayerInfo.js
@@ -2,6 +2,7 @@ class getPlayerInfo extends Phaser.Scene {
     constructor() {
         // Where the player will put their information
         super("playerInfoScreen");
+        this.tempName = 'Alena';
     }
 
     init(data) {
@@ -12,7 +13,7 @@ class getPlayerInfo extends Phaser.Scene {
 
         this.mouseClick = this.sound.add('mouseClick');
 
-        var playerName = this.add.text(10, 10, 'Enter your name:', { font: '32px Courier', fill: '#ffffff' });
+        this.add.text(10, 10, 'Enter your name:', { font: '32px Courier', fill: '#ffffff' });
     
         var playerInput = this.add.text(10, 50, '', { font: '32px Courier', fill: '#ffff00' });
 
@@ -20,29 +21,34 @@ class getPlayerInfo extends Phaser.Scene {
     
         this.input.keyboard.on('keydown', function (event) {
 
+    /*
+        So, here is the functionality:
+        Player types in name then hits enter.
+        Player is asked where they are
+        They type in and hit enter
+        Player is then prompted their destination
+        They type in and hit enter, then the game begins
+    */
+
+        //A document that explains each unique keycode: https://github.com/photonstorm/phaser/blob/v3.51.0/src/input/keyboard/keys/KeyCodes.js 
+        
+
+
+        if (event.keyCode === 8 && playerInput.text.length > 0) { //this is the backspace key; to delete the typed text from playerInput.text string
+            playerInput.text = playerInput.text.substr(0, playerInput.text.length - 1); //delete input
+        }
+        else if (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode < 90)) { // this is to add space and all upper and lower captial letters to the playerInput.text string
+            playerInput.text += event.key; 
+            this.tempName = playerInput.text;
+            this.info.name = this.tempName;
+        }
+
+        
         /*
-            So, here is the functionality:
-            Player types in name then hits enter.
-            Player is asked where they are
-            They type in and hit enter
-            Player is then prompted their destination
-            They type in and hit enter, then the game begins
+        if (event.keyboard == 18){ // this is the alt key
+            playerName.setText(playerInput.value);
+        }
         */
-
-            //A document that explains each unique keycode: https://github.com/photonstorm/phaser/blob/v3.51.0/src/input/keyboard/keys/KeyCodes.js 
-
-            if (event.keyCode === 8 && playerInput.text.length > 0) { //this is the backspace key; 8 = backspace
-                playerInput.text = playerInput.text.substr(0, playerInput.text.length - 1); //delete input
-            }
-            else if (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode < 90)) { // this is the space key
-                playerInput.text += event.key; 
-            }
-
-            /*
-            if (event.keyboard == 18){ // this is the alt key
-                playerName.setText(playerInput.value);
-            }
-            */
         });
         
         // for debug purposes, when you mouse click, it brings you to the game scene
@@ -51,6 +57,12 @@ class getPlayerInfo extends Phaser.Scene {
             this.scene.start('gameScene', this.info);
         }, this);
 
+    }
 
-    }  
+    update() {
+        //console.log('in update: ' + this.tempName);
+        //this.info.name = this.tempName;
+        //console.log('the player name is ' + this.info.name);
+    }
+    
 }
--- src/scenes/splashScreen.js
+++ src/scenes/splashScreen.js
@@ -8,10 +8,11 @@ class splashScreen extends Phaser.Scene {
         this.info = data;
         if(this.info.highestScore == null) { //if there is not a score already
             this.info.highestScore = 0; //set highscore to 0
-            this.name = 'Bob';
-            this.departingLocation = 'San Francisco';
-            this.arrivingLocation = 'New York';
-            console.log('high score in Menu: ' + this.info.highestScore + '. Player name is ' + this.name + ' and they are from ' + this.departingLocation + ' and are going to ' + this.arrivingLocation);
+            this.info.name = 'Bob';
+            this.info.departingLocation = 'San Francisco';
+            this.info.arrivingLocation = 'New York';
+            console.log('high score in Menu: ' + this.info.highestScore + '. Player name is ' + this.info.name + 
+            '. They are from ' + this.info.departingLocation + ' and are going to ' + this.info.arrivingLocation);
         }
 
 
